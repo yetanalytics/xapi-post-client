b934725b365f5da4f18248951ff4f6d527027263
Minoo Kim <minookim@Minoos-MacBook-Pro.local>
2023-06-10 04:22:19 +0900

    drafted error handling

--- src/postclient.clj
+++ src/postclient.clj
@@ -11,29 +11,56 @@
 ;; EDN representation of a sample xapi statement
 (def sample-statement
   {"actor" {"objectType" "Agent"
-            "name" "Admin Minoo"
+            "name" "Admin Eva"
             "mbox" "mailto:admin@example.adlnet.gov"}
    "verb" {"id" "http://adlnet.gov/expapi/verbs/voided"
            "display" {"en-US" "voided"}}
    "object" {"objectType" "StatementRef"
-             "id" "5631dbf0-ebc5-4163-874b-c32088db9fdd"}})
+             "id" "e3612d97-3900-4bef-92fd-d8db73e79e1b"}})
+
+;; EDN representation of a sample xapi statement
+(def error-sample-statement
+  {"actor" {"objectType" "Agent"
+            "name" "Admin ERROR"
+            "mbox" "mailto:admin@example.adlnet.gov"}
+   "verb" {"id" "http://adlnet.gov/expapi/verbs/voided"
+           "display" {"en-US" "voided"}}
+   "object" {"objectType" "StatementRef"
+             "id" "e3612d9wefwefwess-3900-4bef-92fd-d8db73e79e1b"}})
 
 ;; stringfied json formatting of xapi statement
 ; used for testing only
 (def json-statement
-  "{\"object\":{\"id\":\"http://example.com/xapi/activity/simplestatement\",\"definition\":{\"name\":{\"en-US\":\"simple statement\"},\"description\":{\"en-US\":\"A simple Experience API statement. Note that the LRS does not need to have any prior information about the Actor (learner), the verb, or the Activity/object.\"}}},\"verb\":{\"id\":\"http://example.com/xapi/verbs#sent-a-statement\",\"display\":{\"en-US\":\"sent\"}},\"id\":\"c6b212fa-a958-43ce-8f42-99b7d991617e\",\"actor\":{\"mbox\":\"mailto:user@example.com\",\"name\":\"Project Minoo Can API\",\"objectType\":\"Agent\"}}")
+  "{\"object\":{\"id\":\"http://example.com/xapi/activity/simplestatement\",\"definition\":{\"name\":{\"en-US\":\"simple statement\"},\"description\":{\"en-US\":\"A simple Experience API statement. Note that the LRS does not need to have any prior information about the Actor (learner), the verb, or the Activity/object.\"}}},\"verb\":{\"id\":\"http://example.com/xapi/verbs#sent-a-statement\",\"display\":{\"en-US\":\"sent\"}},\"id\":\"e3612d97-3900-4bef-92fd-d8db73e79e1b\",\"actor\":{\"mbox\":\"mailto:user@example.com\",\"name\":\"Project Minoo Can API\",\"objectType\":\"Agent\"}}")
 
-(defn post_statement 
+(defn post_statement_helper 
   [host port key secret statement]
   (client/post (str "http://" host ":" port "/xapi/statements")
                {:basic-auth [key secret]
                 :body (generate-string statement)
                 :headers headers
+                :throw-exceptions false
                 :throw-entire-message? true}))
+
+(defn post_statement
+  [host port key secret statement]
+  (try 
+    (let [resp (post_statement_helper host port key secret statement)]
+      (println (:status resp))
+      (post_statement_helper host port key secret statement))
+   
+       ; status code handling
+       ; if status code is not normal -->
+       ; return exception (?) + message (status code + what's in :body)
+      (catch Exception e
+        (throw (ex-info (str "Error: " (.getMessage e))
+                        {:error e})))))
 
 (comment 
   (println (json/write-str sample-statement))
-  (post_statement "localhost" "8080" "username" "password" sample-statement))
+  (print(str "TESTING: " (post_statement "localhost" "8080" "username" "password" sample-statement)))
+  (post_statement "localhost" "8080" "username" "password" error-sample-statement)
+)
 
 
 
