7de17fc23e19b9efd7b1424a5a474d08c09f49fe
Minoo Kim <minookim@Minoos-MacBook-Pro.local>
2023-06-09 22:38:44 +0900

    posting works, need to work on error handling

--- src/postclient.clj
+++ src/postclient.clj
@@ -1,47 +1,39 @@
 (ns postclient
   (:require [clj-http.client :as client]
             [clojure.string :as str]
-            [cheshire.core :refer :all]))
+            [cheshire.core :refer :all]
+            [clojure.data.json :as json]))
 
 (def headers
   {"Content-Type" "application/json"
-   "X-Experience-API-Version" "1.0.3"})
+   "X-Experience-API-Version" "1.0.3"}) 
 
-(def sample_statement
+;; EDN representation of a sample xapi statement
+(def sample-statement
   {"actor" {"objectType" "Agent"
-            "name" "Example Admin"
+            "name" "Admin Minoo"
             "mbox" "mailto:admin@example.adlnet.gov"}
    "verb" {"id" "http://adlnet.gov/expapi/verbs/voided"
            "display" {"en-US" "voided"}}
    "object" {"objectType" "StatementRef"
-             "id" "e05-aa883-acaf-40ad-bf54-02c8ce485fb0"}})
+             "id" "5631dbf0-ebc5-4163-874b-c32088db9fdd"}})
 
-(def json_statement
-  "{\"object\":{\"id\":\"http://example.com/xapi/activity/simplestatement\",
-  \"definition\":{\"name\":{\"en-US\":\"simple statement\"},\"description\":
-  {\"en-US\":\"A simple Experience API statement. Note that the LRS\\n
-  does not need to have any prior information about the Actor (learner), the\\n
-  verb, or the Activity/object.\"}}},\"verb\":{\"id\":\"http://example.com/xapi
-  /verbs#sent-a-statement\",\"display\":{\"en-US\":\"sent\"}},\"id\":\"fd41c918-
-  b88b-4b20-a0a5-a4c32391aaa0\",\"actor\":{\"mbox\":\"mailto:user@example.com\"
-  ,\"name\":\"Project Tin Can API\",\"objectType\":\"Agent\"}}")
+;; stringfied json formatting of xapi statement
+; used for testing only
+(def json-statement
+  "{\"object\":{\"id\":\"http://example.com/xapi/activity/simplestatement\",\"definition\":{\"name\":{\"en-US\":\"simple statement\"},\"description\":{\"en-US\":\"A simple Experience API statement. Note that the LRS does not need to have any prior information about the Actor (learner), the verb, or the Activity/object.\"}}},\"verb\":{\"id\":\"http://example.com/xapi/verbs#sent-a-statement\",\"display\":{\"en-US\":\"sent\"}},\"id\":\"c6b212fa-a958-43ce-8f42-99b7d991617e\",\"actor\":{\"mbox\":\"mailto:user@example.com\",\"name\":\"Project Minoo Can API\",\"objectType\":\"Agent\"}}")
 
-;; statement is inserted in edn format
 (defn post_statement 
-  [host key secret statement]
-  (client/post (str "http://" host ":8080/xapi/statements")
+  [host port key secret statement]
+  (client/post (str "http://" host ":" port "/xapi/statements")
                {:basic-auth [key secret]
                 :body (generate-string statement)
                 :headers headers
-                :content-type :json
-                :socket-timeout 1000
-                :connection-timeout 1000
-                :accept :json}))
+                :throw-entire-message? true}))
 
-(print (generate-string sample_statement))
-sample_statement
-
-(post_statement "localhost" "username" "password" json_statement)
+(comment 
+  (println (json/write-str sample-statement))
+  (post_statement "localhost" "8080" "username" "password" sample-statement))
 
 
 
