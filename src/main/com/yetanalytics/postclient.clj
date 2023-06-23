670f923e29ce61371b09d686d82f87a7fe4a4b4d
Minoo Kim <minookim@minoos-mbp.lan>
2023-06-23 13:11:49 -0700

    switched from cond to case in error handling

--- src/main/com/yetanalytics/postclient.clj
+++ src/main/com/yetanalytics/postclient.clj
@@ -19,31 +19,25 @@
 (defn post-statement
   [host port key secret statement]
   (try
-    (let [resp (post-statement* host port key secret statement)]
+    (let [resp (post-statement* host port key secret statement)
+          status (:status resp)]
       ; handling error status codes
       ; codes other than 200, 201, 202, 203, 204, 205, 207, 300, 301, 302, 303, 304, 307 
-      ; indicate error
-      (cond 
-        (= (:status resp) 401) 
-        (throw (ex-info (str "Status code: " (:status resp) 
-                             " Reason: Invalid key or secret was entered")
-                        {:type ::auth-error})) 
-        (= (:status resp) 409) 
-        (throw (ex-info (str "Status code: " (:status resp)
-                             " Reason: Cannot insert a duplicate statement")
-                        {:type ::post-error}))
-        (> (:status resp) 307)
-        (throw (ex-info (str "Status code: " (:status resp)
-                             " Reason: " (:body resp))
-                        {:type ::post-error}))
-        :else {}))
+      ; indicate error 
+      (case status
+        401 (throw (ex-info (str "Status code: " (:status resp)
+                                 " Reason: Invalid key or secret was entered")
+                            {:type ::auth-error}))
+        409 (throw (ex-info (str "Status code: " (:status resp)
+                                 " Reason: Cannot insert a duplicate statement")
+                            {:type ::post-error}))
+        (if (> (:status resp) 307)
+          (throw (ex-info (str "Status code: " (:status resp)
+                               " Reason: " (:body resp))
+                          {:type ::post-error})))))
     ;; catching irregular exceptions   
     ;; invalid port and auth exception messages are sent out by clj.http
-     (catch UnknownHostException e
-       (throw (ex-info (str "An invalid hostname was inputted")
-                       {:type ::invalid-host-error}
-                       e)))
-     (catch Exception e
-       (throw (ex-info (str "An unexpected error has occured")
-                       {:type ::functional-error}
-                       e)))))
+    (catch UnknownHostException e
+      (throw (ex-info (str "An invalid hostname was inputted")
+                      {:type ::invalid-host-error}
+                      e)))))
