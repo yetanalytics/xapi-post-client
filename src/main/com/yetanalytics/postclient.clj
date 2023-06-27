78dc9f21b7f1a9b19d46b0407a9b55e5510a0d34
Minoo Kim <minookim@minoos-mbp.lan>
2023-06-27 10:14:36 -0700

    simplified status and body

--- src/main/com/yetanalytics/postclient.clj
+++ src/main/com/yetanalytics/postclient.clj
@@ -20,31 +20,30 @@
 (defn post-statement
   [endpoint key secret statement]
   (try
-    (let [resp (post-statement* endpoint key secret statement)
-          status (:status resp)]
+    (let [{:keys [status body]} (post-statement* endpoint key secret statement)]
       ; handling error status codes
       ; codes other than 200, 201, 202, 203, 204, 205, 207, 300, 301, 302, 303, 304, 307 
       ; indicate error 
       (case status
-        401 (throw (ex-info (str "Status code: " (:status resp)
+        401 (throw (ex-info (str "Status code: " status
                                  " Reason: Invalid key or secret was entered")
                             {:type ::auth-error}))
-        403 (throw (ex-info (str "Status code: " (:status resp)
+        403 (throw (ex-info (str "Status code: " status
                                  " Reason: Cannot access area where permission is not granted")
                             {:type ::forbidden-error}))
-        409 (throw (ex-info (str "Status code: " (:status resp)
+        409 (throw (ex-info (str "Status code: " status
                                  " Reason: Cannot insert a duplicate statement")
                             {:type ::post-error}))
-        (if (and (< (:status resp) 308) (> (:status resp) 299))
-          (throw (ex-info (str "Status code: " (:status resp)
-                               " Reason: " (:body resp))
+        (if (and (< status 308) (> status 299))
+          (throw (ex-info (str "Status code: " status
+                               " Reason: " body)
                           {:type ::redirect-error}))))
       ;; last error conditional
-      (if (> (:status resp) 307)
-        (throw (ex-info (str "Status code: " (:status resp)
-                             " Reason: " (:body resp))
+      (if (> status 307)
+        (throw (ex-info (str "Status code: " status
+                             " Reason: " body)
                         {:type ::post-error})))
-      (:body resp))
+      body)
     ;; catching irregular exceptions   
     ;; invalid port and auth exception messages are sent out by clj.http
     (catch UnknownHostException e
